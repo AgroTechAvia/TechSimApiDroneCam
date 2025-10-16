8851c877c70458ae49ce370ac52826701e11a979
Vladislav <vlad.z030812@gmail.com>
2025-10-16 15:28:03 +0300

    added extra examples

---
+++ examples/square_flight.py
@@ -0,0 +1,55 @@
+from drone_control_api import Drone
+from drone_control_api import PID
+
+import time
+import math
+
+ip = '127.0.0.1'
+port = "1233"
+
+
+def main():
+
+    client = Drone()
+
+    client.connect(ip, port)
+    
+    client.armDrone()
+
+    time.sleep(2.0)
+
+    client.posholdOn()
+
+    time.sleep(2.0)
+
+    client.takeoff()   
+
+    time.sleep(8)
+
+    client.gotoXYdrone(3, 0)
+
+    client.gotoXYdrone(0, 3)
+    
+    client.gotoXYdrone(-3, 0)
+    
+    client.gotoXYdrone(0, -3)
+
+    client.boarding()
+
+    time.sleep(1)
+
+    client.disarmDrone()
+
+    time.sleep(1)
+
+    client.posholdOff()
+
+    client.disconnect()
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
